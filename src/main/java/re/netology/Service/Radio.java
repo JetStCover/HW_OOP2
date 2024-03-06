cb9fc8f57d8b2f51b6cb6e13e4140cc0774487b7
JetStCover <151614082+JetStCover@users.noreply.github.com>
2024-03-06 20:53:35 +0300

    Lombok Changed

--- src/main/java/re/netology/Service/Radio.java
+++ src/main/java/re/netology/Service/Radio.java
@@ -1,15 +1,52 @@
 package re.netology.Service;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor
-@Data
 
 public class Radio {
+    @Getter
     private int currentStation;
+    @Getter
     private int currentVolume;
-    private int allStation;
+    private int allStation = 10;
+
+    public void setCurrentStation(int newCurrentStation) { // выбор станции
+        if (newCurrentStation > 9) {
+            return;
+        }
+        if (newCurrentStation < 0) {
+            return;
+        }
+        this.currentStation = newCurrentStation;
+    }
+
+    public void nextStation() { // переключение на следующую станцию
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation++;
+        }
+    }
+
+    public void prevStation() { // переключение на предыдущую станцию
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation--;
+        }
+    }
+
+    public void increaseVolume() { // Увеличение громкости
+        if (currentVolume < 100) {
+            currentVolume++;
+        }
+    }
+
+    public void downgradeVolume() { // Уменьшение громкости
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
 }
--- src/test/java/re/netology/Service/RadioTest.java
+++ src/test/java/re/netology/Service/RadioTest.java
@@ -16,4 +16,139 @@ class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void shouldOverStation() { // Переключение станции выше 9
+        Radio rad = new Radio();
+
+        rad.setCurrentStation(11);
+
+        int actual = rad.getCurrentStation();
+        int expected = 0;
+
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldUnderStation() { // Переключение станции ниже 0
+        Radio rad = new Radio();
+
+        rad.setCurrentStation(4);
+        rad.setCurrentStation(-1);
+
+        int expected = 4;
+        int actual = rad.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldNextStation() { // Следующая станция
+        Radio rad = new Radio();
+
+        rad.setCurrentStation(7);
+        rad.nextStation();
+
+        int expected = 8;
+        int actual = rad.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNextStationAfter() { // Следующая станция после 9
+        Radio rad = new Radio();
+
+        rad.setCurrentStation(9);
+        rad.nextStation();
+
+        int expected = 0;
+        int actual = rad.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPrevUnderStation() { // Предыдущая станция
+        Radio rad = new Radio();
+
+        rad.setCurrentStation(5);
+        rad.prevStation();
+
+        int expected = 4;
+        int actual = rad.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPrevStation() { // Предыдущая станция ниже 0
+        Radio rad = new Radio();
+
+        rad.setCurrentStation(0);
+        rad.prevStation();
+
+        int expected = 9;
+        int actual = rad.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncVolume() { // Увеличение громкости
+        Radio vol = new Radio();
+
+        vol.increaseVolume();
+
+        int expected = 1;
+        int actual = vol.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldOverIncVolume() { // Увеличение громкости
+        Radio vol = new Radio();
+
+        for (int i = 0; i < 105; i++) {
+            vol.increaseVolume();
+        }
+
+        int expected = 100;
+        int actual = vol.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldDownVolume() { // Уменьшение громкости
+        Radio vol = new Radio();
+
+        vol.downgradeVolume();
+
+        int expected = 0;
+        int actual = vol.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldUnderMinVolume() { // Уменьшение громкости
+        Radio vol = new Radio();
+
+        for (int i = 0; i < 30; i++) {
+            vol.increaseVolume();
+        }
+        for (int i = 0; i < 4; i++) {
+            vol.downgradeVolume();
+        }
+
+        int expected = 26;
+        int actual = vol.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
