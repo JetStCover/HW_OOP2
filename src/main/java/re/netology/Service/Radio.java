8dcb3ca1d0fd41f06192d75f24ec65ba3b8540e6
JetStCover <151614082+JetStCover@users.noreply.github.com>
2024-03-06 20:36:36 +0300

    addedLombok

--- src/main/java/re/netology/Service/Radio.java
+++ src/main/java/re/netology/Service/Radio.java
@@ -12,42 +12,4 @@ public class Radio {
     private int currentStation;
     private int currentVolume;
     private int allStation;
-
-    public void setCurrentStation(int newCurrentStation) { // выбор станции
-        if (newCurrentStation > 9) {
-            return;
-        }
-        if (newCurrentStation < 0) {
-            return;
-        }
-        this.currentStation = newCurrentStation;
-    }
-
-    public void nextStation() { // переключение на следующую станцию
-        if (currentStation == 9) {
-            currentStation = 0;
-        } else {
-            currentStation++;
-        }
-    }
-
-    public void prevStation() { // переключение на предыдущую станцию
-        if (currentStation == 0) {
-            currentStation = 9;
-        } else {
-            currentStation--;
-        }
-    }
-
-    public void increaseVolume() { // Увеличение громкости
-        if (currentVolume < 100) {
-            currentVolume++;
-        }
-    }
-
-    public void downgradeVolume() { // Уменьшение громкости
-        if (currentVolume > 0) {
-            currentVolume--;
-        }
-    }
 }
